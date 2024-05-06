3c39101940db8b1c58231c644da8730c1dff31aa
amantale <amantale@bandwidth.com>
2024-05-06 09:57:36 +0300

    working basic frontend. still need to make the CRUD for both entities and mock login

--- backend/src/main/java/org/backend/controller/StudentController.java
+++ backend/src/main/java/org/backend/controller/StudentController.java
@@ -7,6 +7,7 @@ import org.backend.entities.Student;
 import org.backend.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 @Validated
 @RestController
 @RequestMapping(path = "/student")
+@CrossOrigin("http://localhost:4200")
 public class StudentController {
 
     @Autowired
